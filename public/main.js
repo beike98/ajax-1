062c5e3c6a5a6461fc32695c5b3cea282ae4c6e0
beike98 <49637524+beike98@users.noreply.github.com>
2021-07-17 01:26:47 +0800

    1.0

---
+++ public/main.js
@@ -0,0 +1,70 @@
+//5.JSON
+getJSON.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/5.json");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const Object = JSON.parse(request.response);
+      myName.textContent = Object.name;
+    }
+  };
+  request.send();
+};
+//4.XML
+getCSS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/4.xml");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const dom = request.responseXML;
+      const text = dom.getElementsByTagName("warning")[0].textContent;
+      console.log(text.trim());
+    }
+  };
+  request.send();
+};
+//3.HTML
+getHTML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/3.html");
+  request.onload = () => {
+    const div = document.createElement("div");
+    div.innerHTML = request.response;
+    document.body.appendChild(div);
+  };
+  request.onerror = () => {
+    console.log("失败了");
+  };
+  request.send();
+};
+//2.JS
+getJS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/2.js");
+  request.onload = () => {
+    const script = document.createElement("script");
+    script.innerHTML = request.response;
+    document.body.appendChild(script);
+  };
+  request.onerror = () => {
+    console.log("失败了");
+  };
+  request.send();
+};
+//1.CSS
+getCSS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/1.css");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const style = document.createElement("style");
+        style.innerHTML = request.response;
+        document.head.appendChild(style);
+      } else {
+        alert("加载CSS失败");
+      }
+    }
+  };
+  request.send();
+};
